845afa31d244301b6ad723225ac3dbb66824e0fa
Suman Sapkota <suman2020j@gmail.com>
2024-01-23 23:57:15 +0545

    AuthService Added

--- src/main/java/com/example/project/jwt/service/AuthService.java
+++ src/main/java/com/example/project/jwt/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.project.jwt.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.project.jwt.repository.UserRepository;
+import com.example.project.jwt.utils.AuthResponse;
+import com.example.project.jwt.utils.RegisterRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,7 +14,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 private PasswordEncoder passwordEncoder;
-private final UserRepository userRepo;
+private UserRepository userRepo;
 
+public AuthResponse register(RegisterRequest registerRequest)
+{
+	var user = User.builder()
+			.name(registerRequest.getName())
+			.email(registerRequest.getEmail());
+	}
 
 }
